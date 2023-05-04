fef61b24902b2881b3c30128d6030011020e2b0d
Michael Musil <infi.pc@gmail.com>
2023-05-04 18:59:14 +0200

    init vispr

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import initVispr from "@vispr/runtime";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+initVispr();
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
