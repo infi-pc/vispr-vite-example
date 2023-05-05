59e5303c2139ef80fdffd21e62ae1db10805cecc
Michael Musil <infi.pc@gmail.com>
2023-05-05 09:34:45 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <header className="text-gray-600 body-font">
